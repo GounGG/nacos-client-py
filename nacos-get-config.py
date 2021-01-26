d551b005eea011791753dd9119dbccd5e56a94f6
方记普 <jipu.fang@garena.cn>
2021-01-26 17:58:42 +0800

    修复没更新，也会触发回调函数

--- nacos-get-config.py
+++ nacos-get-config.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding:utf-8
 
-import argparse
-import os, json
-import yaml
+import os
 import shutil
 import subprocess
+from datetime import datetime
 
 import nacos
+import yaml
 from apscheduler.schedulers.blocking import BlockingScheduler
 from dotenv import load_dotenv, find_dotenv
 
@@ -21,6 +21,7 @@ PASSWORD = os.getenv("nacos_auth_passwd")
 
 # auth mode
 client = nacos.NacosClient(SERVER_ADDRESSES, namespace=NAMESPACE, username=USERNAME, password=PASSWORD)
+client.set_options(callback_thread_num=1)
 
 
 class Watcher():
@@ -30,9 +31,11 @@ class Watcher():
     def run(self):
         for p in self.cf['configs']:
             self.cf_path = p['path']
-            self.watch(name=p['name'], id=p['id'], group=p['group'])
+            self.watch(id=p['id'], group=p['group'])
 
     def print_cm(self, status):
+        print("{0} {1} {2} 配置发生变更".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), status['data_id'],
+                                          status['group']))
         snapshot_file = "{0}+{1}+{2}".format(status['data_id'], status['group'], NAMESPACE)
         for p in self.cf['configs']:
             if status['data_id'] == p['id'] and status['group'] == p['group']:
@@ -43,14 +46,14 @@ class Watcher():
                 return False
         return True
 
-    def watch(self, name, id, group):
-        client.add_config_watcher(id, group, self.print_cm, content=name)
+    def watch(self, id, group):
+        client.add_config_watcher(id, group, self.print_cm)
 
 
 if __name__ == '__main__':
     # 传入配置
     with open('config.yaml', 'r+') as f:
-        cf = yaml.load(f, Loader=yaml.FullLoader)
+        cf = yaml.load(f)
 
     # # 常驻调度任务
     scheduler = BlockingScheduler()
